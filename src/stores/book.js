28b1aa585d6a92b99546a794d81f9bb859d41ae3
milo <pannatorn30@gmail.com>
2024-01-02 20:32:20 +0700

    update localstorage

--- src/stores/book.js
+++ src/stores/book.js
@@ -1,15 +1,19 @@
 import { defineStore } from 'pinia'
 
+let booklist = JSON.parse(localStorage.getItem('bookList')) || []
+
 export const useBookStore = defineStore('book', {
   state: () => ({
-  books: []
+  books: booklist
   }),  
   actions: {
    addBook (bookData) {
     this.books.push(bookData)
+    localStorage.setItem('bookList', JSON.stringify(this.books))
    },
    removeBook (bookIndex) {
     this.books.splice(bookIndex,1)
+    localStorage.setItem('bookList', JSON.stringify(this.books))
    },
    editBook (bookData, bookIndex) {
     this.books.splice(bookIndex, 1, bookData)
